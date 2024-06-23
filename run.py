e660e67510161b2b705bc0888e6bfd055d2340a2
Satyanarayan <sns.official.99@gmail.com>
2024-06-23 20:06:31 +0530

    FastAPI added

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from src.main import *
+from fastapi import FastAPI
 
 data = load_data("data/movie_dataset.csv")
 split_data_to_store = split_data(data)
 storing_to_vdb = store_data(split_data_to_store)
 
-movie_name = str(input("Enter a movie name\n"))
-querying = query_data(storing_to_vdb,movie_name)
-print(querying)
+# initializing FastAPI
+app = FastAPI()
+
+
+@app.get("/query")
+async def find_similar_movie(movie:str):
+    result = query_data(vdb=storing_to_vdb,movie_name=movie)
+    return {"output": result}
